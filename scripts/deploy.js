cea7a139bbe5044964ea96aa24656f6d4dce1df2
kazumal <kazuma.hamamoto12@gmail.com>
2022-12-13 10:29:44 +0900

    fix

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,15 +4,20 @@ const main = async () => {
   await nftContract.deployed();
   console.log("contract deplyed to:", nftContract.address);
 
-  let txn = await nftContract.mintToken(
-    "0xAbFA9136C14641036d8c9Ea4Bebb081B51f64267",
-    "ipfs://QmUz6vFVEDuMmj6bqRAGNqzyGvn4KwTW1WaMV918YxzgdB"
-  );
+  const deployer = "0xa54Bb3324F767B2bdBBb640A7A9BC315cbEA9A89";
+  const newOwner = "0xAbFA9136C14641036d8c9Ea4Bebb081B51f64267";
+  const tokenURI1 = "ipfs://QmUz6vFVEDuMmj6bqRAGNqzyGvn4KwTW1WaMV918YxzgdB";
+
+  let txn = await nftContract.transferOwnership(newOwner);
+  await txn.wait();
+  console.log("Owner changed.");
+
+  txn = await nftContract.mintToken(deployer, tokenURI1);
   await txn.wait();
   console.log("Minted NFT #1");
 
   txn = await nftContract.mintToken(
-    "0xAbFA9136C14641036d8c9Ea4Bebb081B51f64267",
+    newOwner,
     "ipfs://Qme7JkBJqYFjuMVGgEPb9Yb1VFcnLkc4ULY9Fr6Un3cLYu"
   );
   await txn.wait();
